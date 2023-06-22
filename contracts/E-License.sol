0fbbb2f917db37a125e1158efc799b45675309d7
SampadSikder <sampadsikder84@gmail.com>
2023-06-22 15:20:20 +0600

    Changes to license

--- contracts/E-License.sol
+++ contracts/E-License.sol
@@ -3,59 +3,82 @@
 pragma solidity >=0.7.0 <0.9.0;
 import "./Brta.sol";
 
-contract ELicense{
-    struct User{
+contract ELicense {
+    struct User {
         string name;
         string licenseNumber;
         string dateOfBirth;
         bool validity;
         string issueingAuthority;
         uint256 grade;
+        address licenseAddress;
     }
 
-    mapping(string=>User) users;
+    mapping(address => User) users;
     BrtaAuthority public brtaContractAddress;
 
-    constructor(address _brtaContractAddress){
-        brtaContractAddress=BrtaAuthority(_brtaContractAddress);
+    constructor(address _brtaContractAddress) {
+        brtaContractAddress = BrtaAuthority(_brtaContractAddress);
     }
 
-
-    modifier Brta(){
-        BrtaAuthority.Brta memory brta=brtaContractAddress.getBrtaOffice(msg.sender);
-        require(brta.brtaKey!=address(0),"Only BRTA authority can access");
+    modifier Brta() {
+        BrtaAuthority.Brta memory brta = brtaContractAddress.getBrtaOffice(
+            msg.sender
+        );
+        require(brta.brtaKey != address(0), "Only BRTA authority can access");
         _;
     }
 
-    function addLicense(string memory _name, string memory _licenseNumber, string memory _dateOfBirth) external payable Brta{
-        BrtaAuthority.Brta memory brta=brtaContractAddress.getBrtaOffice(msg.sender);
-        require(bytes(users[_licenseNumber].licenseNumber).length == 0, "User already exists");
+    function addLicense(
+        string memory _name,
+        string memory _licenseNumber,
+        string memory _dateOfBirth,
+        address _licenseAddress
+    ) external payable Brta {
+        BrtaAuthority.Brta memory brta = brtaContractAddress.getBrtaOffice(
+            msg.sender
+        );
+        require(
+            bytes(users[_licenseAddress].licenseNumber).length == 0,
+            "User already exists"
+        );
 
-        users[_licenseNumber]=User(
+        users[_licenseAddress] = User(
             _name,
             _licenseNumber,
             _dateOfBirth,
             true,
             brta.areaName,
-            1
+            1,
+            _licenseAddress
         );
     }
 
-    function upgradeLicense(string memory _licenseNumber) external payable Brta{
-        require(bytes(users[_licenseNumber].licenseNumber).length != 0, "User already exists");
-        users[_licenseNumber].grade=users[_licenseNumber].grade+1;
+    function upgradeLicense(address _licenseAddress) external payable Brta {
+        require(
+            bytes(users[_licenseAddress].licenseNumber).length != 0,
+            "User already exists"
+        );
+        users[_licenseAddress].grade = users[_licenseAddress].grade + 1;
     }
 
-    function freezeLicense(string memory _licenseNumber) external payable Brta{
-        require(bytes(users[_licenseNumber].licenseNumber).length != 0, "User already exists");
-        users[_licenseNumber].validity=false;
+    function freezeLicense(address _licenseAddress) external payable Brta {
+        require(
+            bytes(users[_licenseAddress].licenseNumber).length != 0,
+            "User already exists"
+        );
+        users[_licenseAddress].validity = false;
     }
 
-    function getELicense(string memory _licenseNumber) public view returns(User memory){
-        return users[_licenseNumber];
+    function unfreezeLicense(address _licenseAddress) external payable Brta {
+        require(
+            bytes(users[_licenseAddress].licenseNumber).length != 0,
+            "User already exists"
+        );
+        users[_licenseAddress].validity = true;
     }
 
-    
-
-
+    function getELicense() public view returns (User memory) {
+        return users[msg.sender];
+    }
 }
